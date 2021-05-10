193dc59767bcc64fc0157ac8fd415f4a63f2596f
Chirag.D <P7112755@ness.com>
2021-05-10 11:48:51 +0530

    Added crud Project

---
+++ Employee crud/springboot-backend/src/main/java/proj/crud/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package proj.crud.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import proj.crud.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
